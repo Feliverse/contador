2d4c4f4dc7892087f52222406e3e898618067df6
Felipe Haybar Valenzuela <114632980+Feliverse@users.noreply.github.com>
2025-09-18 09:35:26 -0400

    First Commit

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = React.useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style= {styles.title}>CONTADOR</Text>
+      <Text style={styles.count}>{count}</Text>
+      <Button style={styles.button} title="Incrementar" onPress={() => setCount(count + 1)} />
+      <Button style={styles.button} title="Disminuir" onPress={() => setCount(count - 1)} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +21,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  count: {
+    fontSize: 48,
+    marginBottom: 20,
+  },
+  button: {
+    margin: 10,
+    padding: 10,
+    borderRadius: 100 / 2,
+    backgroundColor: '#9b4717ff',
+    color: '#af5959ff',
   },
 });
